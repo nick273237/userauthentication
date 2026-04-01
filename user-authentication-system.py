77014283202fd955ac6c088690932fedcb45df95
kubla <nikakublashvili28@gmail.com>
2026-04-01 19:25:41 +0400

    1

---
+++ user-authentication-system.py
@@ -0,0 +1,164 @@
+import sys
+from password_checker import check_password
+
+
+users = [
+    {"username": "bobStookey", "password": "NiNa__gg11"},
+    {"username": "JakeGylly", "password": "EWt_in1@"}
+
+
+]
+
+
+def new_user_validation(username):
+
+    if len(username) < 8 or len(username) > 16:
+        return "invalid length"
+    letter_count = 0
+    for j in range(len(username)):
+        if username[j].isalpha():
+            letter_count += 1
+        else:
+            pass
+    if letter_count < 4:
+        return "invalid alpha"
+
+    for i in username:
+        if i.isspace():
+            return "contains space"
+
+    return "valid"
+
+
+
+def attempts(atts):
+
+    result = 4 - atts
+    if result > 1:
+        print(f"{result} attempts remaining! ")
+    elif result == 1:
+        print(f"{result} attempt remaining! ")
+    else:
+        print("You have been banned! "), sys.exit()
+
+
+
+
+def get_choice():
+    i = 0
+    while True:
+        print("1. Register\n"
+              "2. Log in\n"
+              "3. Quit")
+        choice = input("Please enter your choice: (1, 2, 3): ").strip()
+        if choice not in ["1", "2", "3"]:
+            print("Please enter a valid number")
+            attempts(i)
+            i += 1
+            continue
+        else:
+            if choice == "1":
+                print("---------Welcome!----------")
+                print("---------Rules-------------\n"
+                      "Your username should be more than 8 and less than 16 characters long\n"
+                      "Your username must contain at least 4 alphabetical characters\n"
+                      "Your username must not contain spaces")
+
+                new_username = input("Please choose your username!: ")
+                y = 0
+                while True:
+                    result = new_user_validation(new_username)
+                    if result == "invalid length":
+                        print("Your username should be more than 8 and less than 16 characters long!")
+                    elif result == "invalid alpha":
+                        print("Your username must contain at least 4 alphabetical characters!")
+                    elif result == "contains space":
+                        print("Your username cannot contain spaces!")
+                    else:
+                        taken = False
+                        for user in users:
+                            if user["username"] == new_username:
+                                taken = True
+                                break
+                        if taken:
+                            print("Sorry, that username is already taken! Please choose another one!")
+                            attempts(y)
+                            y += 1
+                            new_username = input("Please choose your username!: ")
+                            continue
+                        else:
+                            print(f"Hello, {new_username}! ")
+                            break
+                    attempts(y)
+                    y += 1
+                    new_username = input("Please choose your username!: ")
+
+                print("---------Create a password-----------")
+                print("---------------RULES-----------------")
+                print("Your password must contain an uppercase letter\n"
+                      "Your password must contain a lowercase letter\n"
+                      "Your password must contain digits\n"
+                      "Your password must not be too repetitive\n"
+                      "Your password must contain a symbol(#, $, @, !, etc...)")
+                k = 0
+                while True:
+                    new_password = input(f"Choose a password for {new_username}: ")
+                    result = check_password(new_password)
+                    if result == "too short":
+                        print("Your password is too short")
+                    elif result == "too repetitive":
+                        print("Your password is too repetitive!")
+                    elif result == "no symbol":
+                        print("Your password must contain symbols! (#, $, @, !, etc...)")
+                    elif result == "no lowercase":
+                        print("Your password must contain a lowercase letter!")
+                    elif result == "no uppercase":
+                        print("Your password must contain an uppercase letter!")
+                    elif result == "no digit":
+                        print("Your password must contain a digit!")
+                    else:
+                        print(f"Great! Your password is: {new_password}")
+                        users.append({
+                            "username": new_username,
+                            "password": new_password
+                            })
+                        break
+
+                    attempts(k)
+                    k += 1
+
+            elif choice == "2":
+                while True:
+                    username = input("What is your username?: ")
+                    found_user = None
+                    for user in users:
+                        if username == user["username"]:
+                            found_user = user
+                            break
+                    if not found_user:
+                        print("We are sorry. We couldn't find your username!")
+                        cont = input("Hit enter if you want to continue or press any key to "
+                                            "get back to the menu! ")
+                        if cont == "":
+                            continue
+                        else:
+                            break
+                    else:
+                        g = 0
+                        while True:
+                            password = input("What is your password?: ")
+                            if password != found_user["password"]:
+                                print("Wrong password!")
+                                attempts(g)
+                                g += 1
+                                continue
+                            else:
+                                print(f"Welcome, {username}! How are you feeling?")
+                                sys.exit()
+
+
+            elif choice == "3":
+                print("It was nice assisting you! Goodbye! ")
+                sys.exit()
+if __name__ == "__main__":
+    get_choice()
